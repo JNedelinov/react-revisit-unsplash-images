12683b1709114f825dddb1e53d7c4963a60852e9
Zhulien Zhivkov <julien.nedelinov7@gmail.com>
2025-10-02 19:31:10 +0300

    fixing the url since there was an issue with the request headers on sending a request to the Unsplash's server

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { useGlobalContext } from '../GlobalContextProvider';
 import api from '../custom';
+import { config } from '../config';
 
 const Gallery = () => {
   const { searchVal } = useGlobalContext();
   const queryFunction = async () => {
-    const res = await api.get('/search/photos', {
-      params: { query: searchVal },
-    });
+    const res = await api.get(
+      `https://api.unsplash.com/search/photos?query=${searchVal}&client_id=${config.accessKey}`
+    );
     return res.data;
   };
 
--- src/custom.js
+++ src/custom.js
@@ -3,11 +3,11 @@ import { config } from './config';
 
 const api = axios.create({
   baseURL: 'https://api.unsplash.com',
-  headers: {
-    Accept: 'application/json',
-    'Accept-Version': 'v1',
-    Authorization: `Client-ID ${config.accessKey}`,
-  },
+  // headers: {
+  //   Accept: 'application/json',
+  //   'Accept-Version': 'v1',
+  //   Authorization: `Client-ID ${config.accessKey}`,
+  // },
 });
 
 export default api;
